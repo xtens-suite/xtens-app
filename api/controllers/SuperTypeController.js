efacdb9995b0a9ac9f67655e02bd162421c4c55e
zigur <massimorgon@gmail.com>
2018-06-12 12:14:23 +0100

    added tests; minor fixes

--- api/controllers/SuperTypeController.js
+++ api/controllers/SuperTypeController.js
@@ -5,13 +5,16 @@
 * @help        :: See http://links.sailsjs.org/docs/controllers
 */
 /* jshint node: true */
-/* globals _, sails, SuperType, SuperTypeService, TokenService, Group, Project */
+/* globals _, sails, SuperTypeService, TokenService */
 "use strict";
 
-const ControllerOut = require("xtens-utils").ControllerOut, ValidationError = require('xtens-utils').Errors.ValidationError;
+const ControllerOut = require("xtens-utils").ControllerOut;
+/*
+const ValidationError = require('xtens-utils').Errors.ValidationError;
 const PrivilegesError = require('xtens-utils').Errors.PrivilegesError;
 const crudManager = sails.hooks.persistence.crudManager;
 const actionUtil = require('sails/lib/hooks/blueprints/actionUtil');
+*/
 const BluebirdPromise = require('bluebird');
 
 const coroutines = {
@@ -19,17 +22,13 @@ const coroutines = {
     getMeta: BluebirdPromise.coroutine(function *(req, res) {
         const operator = TokenService.getToken(req);
         const params = req.allParams();
-        console.log(`SuperTypeController.getMeta - Params: ${JSON.stringify(params)}`);
         const isMultiProject = yield SuperTypeService.isMultiProject(params.id);
-        console.log(`SuperTypeController.getMeta - isMultiProject: ${isMultiProject}`);
         return res.json({
             isMultiProject: isMultiProject
         });
     })
 
 };
-
-
 
 const SuperTypeController = {
 
@@ -41,7 +40,6 @@ const SuperTypeController = {
             return co.error(err);
         });
     }
-
 
 };
 
