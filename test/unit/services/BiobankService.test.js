81b93a749a1723b94c03a2c9a1486aaae6e2e84d
gicaz <nico.zanardi@gmail.com>
2018-10-05 15:26:58 +0200

    uncomment tests

--- test/unit/services/BiobankService.test.js
+++ test/unit/services/BiobankService.test.js
@@ -7,31 +7,31 @@ var expect = chai.expect;
 
 describe('BiobankService', function() {
 
-    // describe('#get', function() {
-    //
-    //     it("should return all biobanks", function(done) {
-    //         var expectedBiobanks = _.cloneDeep(fixtures.biobank);
-    //         var params = {};
-    //         BiobankService.get(params, (err, res) =>{
-    //             expect(res.length).to.eql(expectedBiobanks.length);
-    //             expect(res[0].id).to.eql(expectedBiobanks[0].id);
-    //             done();
-    //             return;
-    //         });
-    //     });
-    //
-    //     it("should return the right biobank", function(done) {
-    //         var expectedBiobank = _.cloneDeep(fixtures.biobank[1]);
-    //         var params = {idBiobanks:"2"};
-    //         BiobankService.get(params, (err, res) =>{
-    //             expect(res.length).to.eql(1);
-    //             expect(res[0].id).to.eql(expectedBiobank.id);
-    //             done();
-    //             return;
-    //         });
-    //     });
-    //
-    // });
+    describe('#get', function() {
+
+        it("should return all biobanks", function(done) {
+            var expectedBiobanks = _.cloneDeep(fixtures.biobank);
+            var params = {};
+            BiobankService.get(params, (err, res) =>{
+                expect(res.length).to.eql(expectedBiobanks.length);
+                expect(res[0].id).to.eql(expectedBiobanks[0].id);
+                done();
+                return;
+            });
+        });
+
+        it("should return the right biobank", function(done) {
+            var expectedBiobank = _.cloneDeep(fixtures.biobank[1]);
+            var params = {idBiobanks:"2"};
+            BiobankService.get(params, (err, res) =>{
+                expect(res.length).to.eql(1);
+                expect(res[0].id).to.eql(expectedBiobank.id);
+                done();
+                return;
+            });
+        });
+
+    });
 
     describe('#getBiobanksToEditProject', function() {
 
