ced5bf3d02426732767ba1d4538a5a8adb070be1
ordet76 <robertocavanna@gaslini.org>
2022-09-14 10:30:47 +0200

    min of undefined

--- api/services/PopulateService.js
+++ api/services/PopulateService.js
@@ -104,11 +104,11 @@ var PopulateService = {
     */
     generateFloatField: function(field) {
 
-        var min = field.min || MIN;
-        var max = field.max || MAX;
+        var min = (field && field.min) || MIN;
+        var max = (field && field.max) || MAX;
         var res = {
             "value": parseFloat(parseFloat(getRandomArbitrary(min,max)).toFixed(3)),
-            "unit": (field.hasUnit) ? field.possibleUnits[0] : undefined
+            "unit": (field && field.hasUnit) ? field.possibleUnits[0] : undefined
         };
         return res;
 
